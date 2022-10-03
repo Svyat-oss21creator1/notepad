1547fe72af785bb1de7d9edaec5cc9236da47a31
Svyat Cherepinsky <svyatoslavcherepinsky@gmail.com>
2022-10-03 21:21:34 +0300

    Notepad working

--- link.rb
+++ link.rb
@@ -1,6 +1,21 @@
 class Link < Post
     def initialize
+        super
+
+        @url = ''        
+    end
+    def read_from_console
+        puts "Adress of link:"
+        @url = STDIN.gets.chomp
+
+        puts "What kind of link?"
+        @text = STDIN.gets.chomp
         
+    end
+    def to_strings
+        time_string = "Created :#{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+        return [@url, @text, time_string]
+         
     end
 
 end
--- task.rb
+++ task.rb
@@ -1,10 +1,23 @@
-class Test < Post
+require 'date'
+class Task < Post
     def initialize
-        super
-        
+        super        
         @due_date = Time.now
     end
+
+    def read_from_console
+        puts "What do you need to do?"
+        @text = STDIN.gets.chomp
+        puts "To what date? Write date in format date, month, year"
+        input = STDIN.gets.chomp
+        @due_date = Date.parse(input)
+        
+    end
+
     def to_strings
+        time_string = "Created :#{@created_at.strftime("%Y.%m.%d, %H:%M:%S")} \n\r \n\r"
+        deadline = "deadline: #{@due_date}"
+        return [deadline, @text, time_string]
         
     end
 end
